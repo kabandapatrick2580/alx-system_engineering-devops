72ed645509dd84c8a7b97ec5c555b3ed4b09142e
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-24 14:55:00 +0200

    Ok

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Kill the "killmenow" process using pkill
 exec { 'killmenow_process':
-command     => 'pkill - killmenow',
-refreshonly => true,
-onlyif      => 'pgrep -f killmenow',
+  command     => 'pkill - killmenow',
+  refreshonly => true,
+  onlyif      => 'pgrep -f killmenow',
 }
