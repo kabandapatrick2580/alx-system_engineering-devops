0299ea1b581070b1db77a547dc1c1bae793f98f8
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-24 14:42:03 +0200

    handled 0-create.file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Define the file resource
 file {'/tmp/school':
-ensure => file,
-mode => '0744',
-owner => 'www-data',
-group => 'ww-data',
-content => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'ww-data',
+  content => 'I love Puppet',
 }
