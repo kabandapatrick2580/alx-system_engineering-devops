f77531c07268b9f9f141e90dfc7b3c5ebf74ae0d
kabandapatrick2580 <sendsome2019@gmail.com>
2023-11-24 14:46:29 +0200

    handled pupet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Define the file resource
-file {'/tmp/school':
-  ensure  => file,
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'ww-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
