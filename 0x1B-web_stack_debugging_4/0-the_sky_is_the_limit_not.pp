dd374c39e879c87cc1c6a74cca9176865776854a
kabandapatrick2580 <sendsome2019@gmail.com>
2024-01-25 18:15:55 +0200

    fx_for-nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increase the number of requests to be handled by nginx
+
+# Let's increase ULIMIT of the default for nginx.
+exec { 'fix-for-nginx':
+  command => '/bin/sed -i "s/15/4096/g" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin'
+}
+
+# Restart the Nginx server
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d'
+}
