8e41b7da29baee62f1c9efd6a78efc8e784ab644
kabandapatrick2580 <sendsome2019@gmail.com>
2024-01-25 18:13:16 +0200

    replaced .format with %

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,16 @@
-# Configure OS settings to allow smooth 
-# login and file operations for the 'holberton' user.
+# Configure user limits for the 'holberton' user.
+
+# Define the base command for adjusting limits
+$limits_command = 'sed -i "/^holberton %{0}/s/{1}/50000/g" /etc/security/limits.conf'
 
 # Adjust hard limits for 'holberton' user
 exec { 'adjust-hard-limits-for-holberton':
-  command => "sed -i '/^holberton hard/s/4/50000/g' /etc/security/limits.conf",
+  command => $limits_command % ['hard', 4],
   path    => '/usr/local/bin:/bin/'
 }
 
 # Adjust soft limits for 'holberton' user
 exec { 'adjust-soft-limits-for-holberton':
-  command => "sed -i '/^holberton soft/s/5/50000/g' /etc/security/limits.conf",
+  command => $limits_command % ['soft', 5],
   path    => '/usr/local/bin:/bin/'
 }
